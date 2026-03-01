48abb2aa940a4e765d46c9afad191ebbd17f2603
MajdNassar0 <majdmnassar05@gmail.com>
2026-03-02 01:23:57 +0200

    feat: add register page UI design

--- Kashop/src/page/register/Register.jsx
+++ Kashop/src/page/register/Register.jsx
@@ -1,12 +1,21 @@
-import { Box, Typography } from '@mui/material'
+import { Box, ButtonBase, TextField, Typography } from '@mui/material'
 import React from 'react'
+import { Button } from 'react-bootstrap'
 
 function Register() {
   return (
    <>
-   <Box className='register-form'>
-    <Typography variant='h1'>Register Page</Typography>
-    <Box component={'form'}>
+   <Box className='register-form' sx={{display:'flex' ,justifyContent:'center', alignItems:'center', flexDirection: 'column'}}>
+    <Typography variant='h3' sx={{marginTop:'50px', marginBottom:'20px'}}>Register Page</Typography>
+    {/* حتى اخلي البوتوم الويدث اللي الها على قدها بعمل هيك في البوكس alignItems:'center */}
+    <Box component={'form'} sx={{display:'flex' ,flexDirection:'column' , width:'50%' , gap:'10px' }}>
+      {/* النيم لازم نلزم فيه في ملف الباك من api نفس الاسماء يكون  */}
+      <TextField id="outlined-basic" label="user name" variant="outlined" />
+      <TextField id="outlined-basic" label="age" variant="outlined" />
+      <TextField id="outlined-basic" label="email" variant="outlined" />
+      <TextField id="outlined-basic" label="password" variant="outlined" />
+      <TextField id="outlined-basic" label="Confirm Password" variant="outlined" />
+      <ButtonBase variant="contained" type='submit' >Register</ButtonBase>
 
     </Box>
    </Box>
